f4764e4a1ebe093b18940a681a68c361e22655eb
ahmadfathy97 <af1381@fayoum.edu.eg>
2021-01-05 02:27:12 +0200

    added input filters

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     api: 'https://tiny-blog-api.herokuapp.com/api'
-    //api: 'http://127.0.0.1:3000/api'
+    // api: 'http://127.0.0.1:3000/api'
   },
   getters:{
     api: state => state.api
